c5427ceceaf4a8c7051ae2d10ad13f7c68cfe439
wilben <459664423@qq.com>
2016-04-03 22:21:26 +0800

    1.显示作品详情、查看大图 2.案例展示

--- app/src/main/java/com/wilben/enddesign/fragment/U_ProjectFragment.java
+++ app/src/main/java/com/wilben/enddesign/fragment/U_ProjectFragment.java
@@ -1,20 +1,56 @@
 package com.wilben.enddesign.fragment;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.ListAdapter;
 
 import com.wilben.enddesign.R;
+import com.wilben.enddesign.activity.MyProjectActivity;
+import com.wilben.enddesign.adapter.ImageAdapter;
 
 public class U_ProjectFragment extends Fragment {
 
+    private String username;
+    private ProgressDialog p;
+    private GridView gv_project;
+    private ListAdapter listAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.u_projectfragment, container, false);
+        View view = inflater.inflate(R.layout.u_projectfragment, container, false);
+        Bundle bundle = getArguments();
+        if (bundle != null) {
+            username = bundle.getString("username");
+        }
+        init(view);
+        p = new ProgressDialog(getActivity());
+        p.setMessage("加载中...");
+        gv_project.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getContext(), MyProjectActivity.class);
+                Bundle bundle = new Bundle();
+                bundle.putString("username", username);
+                bundle.putString("position", String.valueOf(position));
+                intent.putExtras(bundle);
+                startActivity(intent);
+            }
+        });
+        return view;
+    }
+
+    private void init(View view) {
+        gv_project = (GridView) view.findViewById(R.id.gv_project);
+        listAdapter = new ImageAdapter(getActivity());
+        gv_project.setAdapter(listAdapter);
     }
 
 
